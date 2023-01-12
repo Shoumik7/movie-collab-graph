7d208c801e24e4e235845a83530e86195ef2bce2
Advait <advait.gosai@gmail.com>
2023-01-13 00:58:17 +0530

    Top 1000 Movies by Rating API Call

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  const [data, setData] = useState({data: []});
+  const [movieData, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState('');
 
@@ -11,36 +12,34 @@ function App() {
     setIsLoading(true);
     try {
       const apiKey = "4dc47b6a53bc5c22c68471515de658c0";
-      const {data} = await axios.get('https://reqres.in/api/users');
-      console.log('data is: ', JSON.stringify(data, null, 4));
-
-      setData(data);
+      let allMovies = "";
+      for (let i = 1; i <= 50; i++) {
+        let resp = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKey + "&language=en-US&page=" + i.toString());
+        for (let i = 0; i < 20; i++) {
+          let currMovie = resp.data.results[i].title + "\n";
+          allMovies += currMovie;
+          console.log(currMovie);
+        }
+      }
+      setData(allMovies);
     } catch (err) {
       setErr(err.message);
     } finally {
       setIsLoading(false);
     }
   };
-
-  console.log(data);
   
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {err && <h2>{err}</h2>}
+        <button onClick={handleClick}>Fetch data</button>
+        {isLoading && <h2>Loading...</h2>}
+      </div>
+      <div>
+        <p>{movieData}</p>
+      </div>
     </div>
   );
 }
