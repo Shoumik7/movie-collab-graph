b202c3fbb08dd30bc1dd5a0f346615c87e7124be
Advait <advait.gosai@gmail.com>
2023-01-13 00:15:57 +0530

    Axios Setup

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState({data: []});
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const apiKey = "4dc47b6a53bc5c22c68471515de658c0";
+      const {data} = await axios.get('https://reqres.in/api/users');
+      console.log('data is: ', JSON.stringify(data, null, 4));
+
+      setData(data);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log(data);
+  
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
