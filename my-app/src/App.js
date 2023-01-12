d15f747121e6d32b75e05ffd19fea47c057d57c9
nealshuh <nealmshah11@gmail.com>
2023-01-12 17:01:24 -0500

    First sign of graph using Sigma
    Added graph and separate page for updating sigma graph, soon to be incorporated with more modals and such

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
+import MyGraph from './pages/MyGraph'
 
 function App() {
   const [data, setData] = useState({data: []});
@@ -22,26 +25,13 @@ function App() {
     }
   };
 
-  console.log(data);
-  
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MyGraph />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
