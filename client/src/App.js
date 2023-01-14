8a62df1fb3a340794bf2454627b202840a8ee087
Sai Sompally <ssompally@gmail.com>
2023-01-14 07:17:23 -0500

    event starting

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,6 @@ import { parse } from "graphology-gexf/browser";
 import { useRegisterEvents } from "@react-sigma/core";
 import { FC } from "react";
 
-
-
 export const LoadGraph = () => {
   const loadGraph = useLoadGraph();
 
@@ -82,55 +80,28 @@ function App() {
 
   const graph = Graph.from(testJson);
 
-  const GraphEvents: React.FC = () => {
+  const [hoveredNode, setHoveredNode] = useState("");
+  const [selectedNode, setSelectedNode] = useState("");
+  const [hoveredNeighbors, setHoveredNeighbors] = useState([]);
+
+  const GraphEvents = () => {
     const registerEvents = useRegisterEvents();
 
     useEffect(() => {
       // Register the events
       registerEvents({
         // node events
-        clickNode: (event) => console.log("clickNode", event.event, event.node, event.preventSigmaDefault),
-        doubleClickNode: (event) => console.log("doubleClickNode", event.event, event.node, event.preventSigmaDefault),
-        rightClickNode: (event) => console.log("rightClickNode", event.event, event.node, event.preventSigmaDefault),
-        wheelNode: (event) => console.log("wheelNode", event.event, event.node, event.preventSigmaDefault),
-        downNode: (event) => console.log("downNode", event.event, event.node, event.preventSigmaDefault),
-        enterNode: (event) => console.log("enterNode", event.node),
-        leaveNode: (event) => console.log("leaveNode", event.node),
-        // edge events
-        clickEdge: (event) => console.log("clickEdge", event.event, event.edge, event.preventSigmaDefault),
-        doubleClickEdge: (event) => console.log("doubleClickEdge", event.event, event.edge, event.preventSigmaDefault),
-        rightClickEdge: (event) => console.log("rightClickEdge", event.event, event.edge, event.preventSigmaDefault),
-        wheelEdge: (event) => console.log("wheelEdge", event.event, event.edge, event.preventSigmaDefault),
-        downEdge: (event) => console.log("downEdge", event.event, event.edge, event.preventSigmaDefault),
-        enterEdge: (event) => console.log("enterEdge", event.edge),
-        leaveEdge: (event) => console.log("leaveEdge", event.edge),
-        // stage events
-        clickStage: (event) => console.log("clickStage", event.event, event.preventSigmaDefault),
-        doubleClickStage: (event) => console.log("doubleClickStage", event.event, event.preventSigmaDefault),
-        rightClickStage: (event) => console.log("rightClickStage", event.event, event.preventSigmaDefault),
-        wheelStage: (event) => console.log("wheelStage", event.event, event.preventSigmaDefault),
-        downStage: (event) => console.log("downStage", event.event, event.preventSigmaDefault),
-        // default mouse events
-        click: (event) => console.log("click", event.x, event.y),
-        doubleClick: (event) => console.log("doubleClick", event.x, event.y),
-        wheel: (event) => console.log("wheel", event.x, event.y, event.delta),
-        rightClick: (event) => console.log("rightClick", event.x, event.y),
-        mouseup: (event) => console.log("mouseup", event.x, event.y),
-        mousedown: (event) => console.log("mousedown", event.x, event.y),
-        mousemove: (event) => console.log("mousemove", event.x, event.y),
-        // default touch events
-        touchup: (event) => console.log("touchup", event.touches),
-        touchdown: (event) => console.log("touchdown", event.touches),
-        touchmove: (event) => console.log("touchmove", event.touches),
-        // sigma kill
-        kill: () => console.log("kill"),
-        // sigma camera update
-        updated: (event) => console.log("updated", event.x, event.y, event.angle, event.ratio),
+        //clickNode: (event) => console.log("clickNode", event.event, event.node, event.preventSigmaDefault),
+        //enterNode: (event) => {console.log("enterNode", event.node); setHoveredNode(event.node)},
+        enterNode: (event) => {console.log("enterNode", event.node)}
+        //leaveNode: (event) => console.log("leaveNode", event.node),
       });
     }, [registerEvents]);
 
     return null;
   };
+
+  
 
 
   return (
@@ -155,21 +126,22 @@ function App() {
           labelGridCellSize: 60,
           labelRenderedSizeThreshold: 20,
           labelFont: "Lato, sans-serif",
-          zIndex: true,
-          maxNodeSize: 0.2
-          
+          zIndex: true,          
         }}
+        
       >
+        <GraphEvents />
+        
         <ControlsContainer position={"bottom-right"}>
           <ZoomControl />
           <FullScreenControl />
           <LayoutForceAtlas2Control />
         </ControlsContainer>
-        <ControlsContainer position={"top-right"}>
-          <SearchControl style={{ width: "200px" }} />
+        <ControlsContainer position={"top-left"}>
+          <SearchControl style={{ width: "350px" }} />
         </ControlsContainer>
 
-        <GraphEvents />
+        
       </SigmaContainer>
 
         
