0faccf5c3ab09ea2e3ae83346cf24daa55547f56
nealshuh <nealmshah11@gmail.com>
2023-01-14 20:56:35 -0500

    Readded searchbars and sidebars

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Graph from "graphology";
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
 import "@react-sigma/core/lib/react-sigma.min.css";
 import getNodeProgramImage from "sigma/rendering/webgl/programs/node.image";
-import {ControlsContainer, ZoomControl, FullScreenControl, SearchControl } from "@react-sigma/core";
+import { ControlsContainer, ZoomControl, FullScreenControl, SearchControl } from "@react-sigma/core";
 import { LayoutForceAtlas2Control } from "@react-sigma/layout-forceatlas2";
 import testJson from "./test.json";
 import testJsonGraphFromGexf from "./testoutput.json";
@@ -16,6 +16,8 @@ import newTestJson from "./newTestJson.json";
 import { parse } from "graphology-gexf/browser";
 import { useRegisterEvents, useSetSettings, useSigma } from "@react-sigma/core";
 import { FC } from "react";
+import Button, { ButtonProps } from '@mui/material/Button';
+import { Drawer, Box } from '@mui/material';
 
 
 export const LoadGraph = () => {
@@ -40,6 +42,7 @@ function App() {
   const [err, setErr] = useState('');
   const [addedCollab, setAddedCollab] = useState(false);
   const [showWelcome, setShowWelcome] = useState(true);
+
   let [movieArr, setMovieArr] = useState([]);
   const movieItems = movieArr.map((movie) =>
     <li key={movie.toString()}>
@@ -47,56 +50,105 @@ function App() {
     </li>
   );
 
-  /*
-  const handleClick = async () => {
-    setIsLoading(true);
-    try {
-      const apiKey = "4dc47b6a53bc5c22c68471515de658c0";
-      let allMovies = [];
-      for (let i = 1; i <= 50; i++) {
-        let resp = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKey + "&language=en-US&page=" + i.toString());
-        for (let i = 0; i < 20; i++) {
-          let currMovie = resp.data.results[i].title;
-          allMovies.push(currMovie);
-          let actorResp = await axios.get("https://api.themoviedb.org/3/movie/" + resp.data.results[i].id + "?api_key=" + apiKey + "&language=en-US");
-          
-        }
-      }
-      
-      setData(allMovies);
-    } catch (err) {
-      setErr(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-  */
-
-  /*
-  const [graph, setGraph] = useState(0);
-
-  useLayoutEffect(() => {
-    fetch("./test.gexf")
-      .then((res) => res.text())
-      .then((gexf) => {
-        setGraph(parse(Graph, gexf))
-        console.log(graph)
-      })
-
-  }, []);
-  */
-
-  //const graph = Graph.from(jsonGraphExample);
-  //const graph = {"nodes":[{"id":"0","label":"0"},{"id":"1","label":"1"},{"id":"2","label":"2"},{"id":"3","label":"3"}],"edges":[{"id":"0","type":"undirected","label":"","source":"0","target":"1","weight":1},{"id":"1","type":"undirected","label":"","source":"1","target":"2","weight":1},{"id":"2","type":"undirected","label":"","source":"2","target":"3","weight":1}]}
-  //const graph = GraphFormatConverter.fromJson(jsonGraph);
-
-  //fixNG = json_graph.node_link_data(NG)
 
   const graph = Graph.from(testJson);
 
   const [hoveredNode, setHoveredNode] = useState("");
   const [selectedNode, setSelectedNode] = useState("");
   const [hoveredNeighbors, setHoveredNeighbors] = useState([]);
+
+  const SearchBar = () => {
+    //const { gotoNode } = useCamera();
+    // Search value
+    const [search, setSearch] = useState("");
+    // Datalist values
+    const [values, setValues] = useState([ {} ]);
+    // Selected
+    const [selected, setSelected] = useState(null);
+    // random id for the input
+    const [inputId, setInputId] = useState("");
+
+  /**
+   * When component mount, we set a random input id.
+   */
+  useEffect(() => {
+    setInputId(`search-${0}`);
+  }, []);
+
+  /**
+   * When the search input changes, recompute the autocomplete values.
+   */
+  useEffect(() => {
+    const newValues = [];
+    if (!selected && search.length > 1) {
+      graph.forEachNode((key, attributes) => {
+        if (attributes.label && attributes.label.toLowerCase().includes(search.toLowerCase()))
+          newValues.push({ id: key, label: attributes.label });
+      });
+    }
+    setValues(newValues);
+  }, [search]);
+
+
+  /**
+   * When the selected item changes, highlighted the node and center the camera on it.
+   */
+  useEffect(() => {
+    if (!selected) {
+      return;
+    }
+
+    graph.setNodeAttribute(selected, "highlighted", true);
+    //gotoNode(selected);
+    localStorage.setItem("clickedNode", selected)
+
+    return () => {
+      graph.setNodeAttribute(selected, "highlighted", false);
+    };
+  }, [selected]);
+
+  /**
+   * On change event handler for the search input, to set the state.
+   */
+  const onInputChange = (e) => {
+    const searchString = e.target.value;
+    const valueItem = values.find((value) => value.label === searchString);
+    if (valueItem) {
+      setSearch(valueItem.label);
+      setValues([]);
+      setSelected(valueItem.id);
+    } else {
+      setSelected(null);
+      setSearch(searchString);
+    }
+  };
+
+  // Common html props for the div
+
+  return (
+    <div style={{ width: "200px"}}>
+      <label htmlFor={inputId} style={{ display: "none" }}>
+          Search a node
+        </label>
+        <input
+          id={inputId}
+          type="text"
+          placeholder="Search for actors and directors..."
+          list={`${inputId}-datalist`}
+          value={search}
+          onChange={onInputChange}
+          style={{width: "190px"}}
+        />
+        <datalist id={`${inputId}-datalist`}>
+          {values.map((value) => (
+            <option key={value.id} value={value.label}>
+              {value.label}
+            </option>
+          ))}
+        </datalist>
+      </div>
+    );
+  };
 
   const GraphEvents =  () => {
     const sigma = useSigma();
@@ -106,6 +158,7 @@ function App() {
     const [hoveredNode, setHoveredNode] = useState(null);
     const [secondHoveredNode, setSecondHoveredNode] = useState(null);
     const [clickedNode, setClickedNode] = useState(null);
+
 
     useEffect(() => {
       // Register the events
@@ -244,7 +297,9 @@ function App() {
     }, [secondHoveredNode])
   
     return null;
-  }  
+  }
+
+
 
   return (
     <div>
@@ -265,17 +320,28 @@ function App() {
       >
         <GraphEvents />
         
-        <ControlsContainer position={"bottom-right"}>
-          <ZoomControl />
-          <FullScreenControl />
-          <LayoutForceAtlas2Control />
+        <ControlsContainer style={{ marginLeft: "10px", marginTop: "10px"}} position={"top-left"}>
+          <SearchBar />
         </ControlsContainer>
-        <ControlsContainer position={"top-left"}>
-          <SearchControl style={{ width: "350px" }} />
+        <ControlsContainer style={{ marginLeft: "10px", marginTop: "55px"}} position={"top-left"}>
+          <SearchBar />
         </ControlsContainer>
-
+        <ControlsContainer style={{ border: "0px", marginLeft: "130px", marginTop: "95px"}} position={"top-left"}>
+          
+        </ControlsContainer>
       </SigmaContainer>
 
+      <Drawer variant="permanent" hideBackdrop anchor = 'right' open>
+        <Box
+          sx={{ width: 400 }}
+          role="presentation"   
+        >
+          <ul>
+            {showWelcome && <h1 margin="10px">Welcome to Movie Collabs!</h1> }
+            {movieItems}
+          </ul>
+        </Box>
+      </Drawer>
         
     </div>
   );
